f78889aa2aaf83326a2676250c02541aa7d49208
Austin Leach <austinLeach850@gmail.com>
2020-10-06 17:05:43 -0400

    documentation of astronaut script

--- DinoGame/Assets/Scripts/Astronaut.cs
+++ DinoGame/Assets/Scripts/Astronaut.cs
@@ -7,11 +7,12 @@ using UnityEngine.SceneManagement;
 public class Astronaut : MonoBehaviour
 {
 
+    //allows us to change the camera settings later
     public Cinemachine.CinemachineVirtualCamera playerCamera;
 
     Rigidbody2D rigidBody2D;
 
-    float horizontal;
+    float horizontal; 
     float vertical;
 
 
@@ -25,11 +26,12 @@ public class Astronaut : MonoBehaviour
     void Update()
     {
         //movement
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");   //value from -1 to 1 that takes input from A and D or leftArrow and rightArrow
+        vertical = Input.GetAxis("Vertical");   //value from -1 to 1 that takes input from W and S or upArrow and downArrow
     }
 
     void FixedUpdate() {
+        // updates the position of the character based off of the values we recieved from Input
         Vector2 position = rigidBody2D.position;
         position.x = position.x + 10f * horizontal * Time.deltaTime;
         position.y = position.y + 10f * vertical * Time.deltaTime;
