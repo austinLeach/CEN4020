7736ac8e8aa0a84c8188150ff294b8513b017569
Austin Leach <austinLeach850@gmail.com>
2020-10-09 02:05:35 -0400

    babyDino was on the wrong side of the player

--- DinoGame/Assets/Scripts/babyDino.cs
+++ DinoGame/Assets/Scripts/babyDino.cs
@@ -29,10 +29,10 @@ public class babyDino : MonoBehaviour
             // acquires target to move towards and then does so at a specified speed
             target = astronaut.transform;
             if (astronaut.direction.x > 0f) {
-                positioning = 2.5f;
+                positioning = -1.5f;
             }
             else {
-                positioning = -1.5f;
+                positioning = 2.5f;
             }
             // target.position.y - 1 is so it is at the feet of the astronaut
             transform.position = Vector3.MoveTowards(transform.position, new Vector2(target.position.x + positioning, target.position.y - 1.1f), speed * Time.deltaTime);
