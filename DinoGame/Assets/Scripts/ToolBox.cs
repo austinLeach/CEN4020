10271b5ad0505a3a04db4631dd48a99ab9c85667
Marc Montero <mam17k@my.fsu.edu>
2020-10-26 17:44:38 -0400

    Tool box script added
    Working version of the script for the tool box game object has been added to the project

---
+++ DinoGame/Assets/Scripts/ToolBox.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolBox : MonoBehaviour
+{
+    Astronaut astronaut;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D obj)
+    {
+        astronaut = obj.GetComponent<Astronaut>();
+        // if the object that is colliding with the ToolBox is an astronaut
+        if(astronaut)
+        {
+            // Display message on screen to congratulate player for completing the puzzle
+
+            Destroy(gameObject);  // removes the toolbox from the scene to signify that it has been collected already
+        }
+    }
+}
