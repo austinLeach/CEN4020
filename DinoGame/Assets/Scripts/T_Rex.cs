ba107c3e44dd759f0e6191a846f5b9625a280676
JimenezPlusPlus <raj18bl@my.fsu.edu>
2020-11-04 23:19:58 -0500

    Still working on T_Rex.cs

--- DinoGame/Assets/Scripts/T_Rex.cs
+++ DinoGame/Assets/Scripts/T_Rex.cs
@@ -6,39 +6,45 @@ using UnityEngine;
 public class T_Rex : MonoBehaviour
 {
   //player object
-  GameObject target;
+  public Astronaut astronaut;
+  Transform target;
   //Transform object for start position
-  Transform initPos;
+  Vector2 start;
   //Vector Obbjects for movement
-  Vector2 strtDir;//start direction of T-Rex
-  Vector2 roamDir;//moving direction of T-Rex
   Rigidbody2D rigidBody2D;
   //T-Rex variables
-  private float speed = 11f;//movement speed
+  public float speed = 11f;//movement speed
   private float initRange = 10f;//range of triggering chase.
   private float chaseRange = 20f;//range T-Rez will chase for.
   private bool chase = false;//determines if T-Rex is chasing player
 
   void Start(){
+    astronaut = astronaut.GetComponent<Astronaut>();
     rigidBody2D = GetComponent<Rigidbody2D>();
-    target = GameObject.FindGameObjectWithTag("Player");
-    initPos.position = Vector2.zero;
+    start = transform.position;
   }
 
   void Update(){
+    //
+  }
+
+  void FixedUpdate(){
+    target = astronaut.transform;
     //if player comes too close to T-Rex, trigger chase(change to fight later)
-    if((initPos.position - target.transform.position).magnitude < initRange){
+    float a = Vector3.Distance(astronaut.transform.position, start);
+    float b = Vector3.Distance(astronaut.transform.position, start);
+    if( a < initRange){
       chase = true;
     }
-    else if((initPos.position - target.transform.position).magnitude > chaseRange){
+    //T-Rex stops chasing player at a certain range
+    if(b > chaseRange){
       chase = false;
     }
-    //T-Rex has a leashed range; won't chase indefinitely
     if(chase){
-      transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+      transform.position = Vector3.MoveTowards(transform.position, new Vector2(target.position.x, target.position.y), speed * Time.deltaTime);
     }
-    else{//player out of chase range; return to start position
-      transform.position = Vector2.MoveTowards(transform.position, initPos.transform.position, speed * Time.deltaTime);
+    else{
+      transform.position = Vector3.MoveTowards(transform.position, start, speed * Time.deltaTime);
     }
   }
 }
