50e39cbda53ba8b9a72b99812a7707034f10f56a
JimenezPlusPlus <raj18bl@my.fsu.edu>
2020-11-06 23:21:36 -0500

    Adding triceratops.
    Added artwork for triceratops sprite. Set two into world and both will chase you like the T-Rex. I am attempting to generalize the combat code, so that different dinosaurs load combat with different stats.

--- DinoGame/Assets/Scripts/Combat.cs
+++ DinoGame/Assets/Scripts/Combat.cs
@@ -5,14 +5,13 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public enum combatStates { PlayersTurn, EnemiesTurn, Win, Loss }
-public enum FightingWith { TRex }; // can be added on to for more combat scenarios
+public enum FightingWith { dino }; // can be added on to for more combat scenarios
 public class Combat : MonoBehaviour
 {
-
-    public Slider bossHealthSlider;
+    public Slider dinoHealthSlider;
     public Slider playerHealthSlider;
 
-    public GameObject Trex;
+    public GameObject dino;
 
     bool stunned = false;
 
@@ -20,18 +19,27 @@ public class Combat : MonoBehaviour
 
     public FightingWith fighting;
 
-
-
-
     // Start is called before the first frame update
     void Start()
     {
         currentState = combatStates.PlayersTurn;
-        if (GlobalVariables.FightingWith == "TRex") {
-            setMaxBossHealth(10);
-            Trex.GetComponent<Renderer>().enabled = true;   // used so the Trex sprite will render
+        switch (GlobalVariables.FightingWith){
+          case "TRex":
+            setMaxDinoHealth(25);
+            dino.GetComponent<Renderer>().enabled = true;
+            break;
+          case "Tricera":
+            setMaxDinoHealth(20);
+            dino.GetComponent<Renderer>().enabled = true;
+            break;
+          case "Veloc":
+            setMaxDinoHealth(10);
+            dino.GetComponent<Renderer>().enabled = true;
+            break;
+          default:
+            break;
         }
-        
+
         setMaxPlayerHealth(10);
     }
 
@@ -39,31 +47,31 @@ public class Combat : MonoBehaviour
     void FixedUpdate()
     {
         if (currentState == combatStates.Win) {
-            GlobalVariables.TRexDefeated = true;
+            GlobalVariables.dinoDefeated = true;
             SceneManager.LoadScene("SampleScene");
         }
         if (currentState == combatStates.Loss) {
             SceneManager.LoadScene("Combat");
         }
-        
+
     }
 
     void PlayerAttacks(int damage) {
-        setBossHealth(damage);
+        setDinoHealth(damage);
 
-        if (bossHealthSlider.value <= 0) {
+        if (dinoHealthSlider.value <= 0) {
             currentState = combatStates.Win;
         }
         else {
             currentState = combatStates.EnemiesTurn;
-            if (fighting == FightingWith.TRex)
-                StartCoroutine(TRexTurn());
+            if (fighting == FightingWith.dino)
+                StartCoroutine(dinoTurn());
         }
     }
 
-    IEnumerator TRexTurn() {
+    IEnumerator dinoTurn() {
         yield return new WaitForSeconds(1f);
-       
+
         if (!stunned) {
             setPlayerHealth(1); // default attack
         }
@@ -77,13 +85,13 @@ public class Combat : MonoBehaviour
         }
     }
 
-    public void setMaxBossHealth(int health) {
-        bossHealthSlider.maxValue = health;
-        bossHealthSlider.value = health;
+    public void setMaxDinoHealth(int health) {
+        dinoHealthSlider.maxValue = health;
+        dinoHealthSlider.value = health;
     }
 
-    public void setBossHealth(int health) {
-        bossHealthSlider.value -= health;
+    public void setDinoHealth(int health) {
+        dinoHealthSlider.value -= health;
     }
 
     public void setMaxPlayerHealth(int health) {
@@ -96,8 +104,26 @@ public class Combat : MonoBehaviour
     }
 
     public void Stun() {
-        if (Random.Range(0, 100) < 90) 
+      float s = Random.Range(0, 100);
+      switch (GlobalVariables.FightingWith){
+        case "TRex":
+          if(s < 50){
             stunned = true;
+          }
+          break;
+        case "Tricera":
+          if(s < 40){
+            stunned = true;
+          }
+          break;
+        case "Veloc":
+          if(s < 80){
+            stunned = true;
+          }
+          break;
+        default:
+          break;
+      }
     }
 
     public void FireBall() {
