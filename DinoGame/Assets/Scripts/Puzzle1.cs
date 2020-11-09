b3ea5270013d7e76d9c336ddc91c001dda53a3cd
Marc Montero <mam17k@my.fsu.edu>
2020-11-09 13:34:05 -0500

    Puzzle script added
    script to display the puzzle has been added

---
+++ DinoGame/Assets/Scripts/Puzzle1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Puzzle1 : MonoBehaviour
+{
+    public string puzzle;
+    public GameObject textDisplay;
+
+    public void DisplayPuzzle()
+    {
+        puzzle = "Hint: the tool box is hidden within an environment where leaves may coexist to form one entity.";
+        textDisplay.GetComponent<Text>().text = puzzle;
+    }
+}
